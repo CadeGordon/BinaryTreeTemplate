8d62b6a326773fb13ed4ad8d193473cb47368913
Cade Gordon <cadegordon093@gmail.com>
2022-02-02 16:57:35 -0600

    started function in binARY tree

--- CDDS_BinaryTree/BinaryTree.h
+++ CDDS_BinaryTree/BinaryTree.h
@@ -51,3 +51,63 @@ private:
 };
 
 #endif
+
+
+
+template<typename T>
+inline void BinaryTree<T>::draw(TreeNode<T>* currentNode, int x, int y, int horizontalSpacing, TreeNode<T>* selected)
+{
+	//sets the needed space in between 
+	horizontalSpacing /= 2;
+	
+	//check if the ucurrent node in null
+	if (currentNode)
+	{
+		//draws the left cilkd if this node has one
+		if (currentNode->hasLeft())
+		{
+			//Darws a line betwen the left child and the ucrrent node
+			DrawLine(x, y, x - horizontalSpacing, y + 80, RED);
+			//dras the left child
+			draw(currentNode->getLeft(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+
+		//draws the right child if this nod has one
+		if (currentNode->hasRight())
+		{
+			//draws a line between this child and the curent code
+			DrawLine(x, y, x + horizontalSpacing, y + 80, RED);
+			//draws the right child
+			draw(currentNode->getright(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+		//draws the current node
+		currentNode->draw(x, y, x(selected == currentNode));
+
+	}
+}
+
+template<typename T>
+inline BinaryTree<T>::BinaryTree()
+{
+}
+
+template<typename T>
+inline BinaryTree<T>::~BinaryTree()
+{
+}
+
+template<typename T>
+inline bool BinaryTree<T>::isEmpty() const
+{
+	return false;
+}
+
+template<typename T>
+inline void BinaryTree<T>::insert(T value)
+{
+}
+
+template<typename T>
+inline void BinaryTree<T>::remove(T value)
+{
+}
