b793b28dbdcf9e2cbb8606fae1b8dcc1eca1e871
Cade Gordon <cadegordon093@gmail.com>
2022-02-04 09:57:02 -0600

    fixed most errors but external error is still issue

--- CDDS_BinaryTree/BinaryTree.h
+++ CDDS_BinaryTree/BinaryTree.h
@@ -10,30 +10,30 @@ class BinaryTree
 {
 public:
 
-	BinaryTree() {};
-	~BinaryTree() {};
+	BinaryTree();
+	~BinaryTree();
 
 	/// <summary>
 	/// Returns whether or not there are any nodes in the list
 	/// </summary>
-	bool isEmpty() {} const;
+	bool isEmpty() const;
 	/// <summary>
 	/// Creates a new node that stores the given value and places it into the tree
 	/// </summary>
 	/// <param name="value">The new value to add to the tree</param>
-	void insert(T value) {};
+	void insert(T value);
 	/// <summary>
 	/// Finds the node with the given value and removes it from the tree
 	/// </summary>
 	/// <param name="value">The value of the node to search for in the tree</param>
-	void remove(T value) {};
+	void remove(T value);
 	/// <summary>
 	/// Finds and returns a node with the given value in the tree
 	/// </summary>
 	/// <param name="value">The value of the node to search for</param>
 	TreeNode<T>* find(T value) {return nullptr;}
 
-	void draw(TreeNode<T>* selected = nullptr) {}
+	void draw(TreeNode<T>* selected = nullptr);
 
 private:
 	/// <summary>
@@ -43,48 +43,14 @@ private:
 	/// <param name="nodeFound">A pointer that will store the address of the node that was found</param>
 	/// <param name="nodeParent">A pointer that will store the address of the parent of the node that was found</param>
 	/// <returns>Whether or not a node matching the value could be found</returns>
-	bool findNode(T searchValue, TreeNode<T>*& nodeFound, TreeNode<T>*& nodeParent) {}
+	bool findNode(T searchValue, TreeNode<T>*& nodeFound, TreeNode<T>*& nodeParent);
 
-	void draw(TreeNode<T>*, int x, int y, int horizontalSpacing, TreeNode<T>* selected = nullptr) {}
+	void draw(TreeNode<T>*, int x, int y, int horizontalSpacing, TreeNode<T>* selected = nullptr);
 
 	TreeNode<T>* m_root = nullptr;
 };
 
 #endif
-
-
-
-template<typename T>
-inline void BinaryTree<T>::draw(TreeNode<T>* currentNode, int x, int y, int horizontalSpacing, TreeNode<T>* selected)
-{
-	//sets the needed space in between 
-	horizontalSpacing /= 2;
-	
-	//check if the ucurrent node in null
-	if (currentNode)
-	{
-		//draws the left cilkd if this node has one
-		if (currentNode->hasLeft())
-		{
-			//Darws a line betwen the left child and the ucrrent node
-			DrawLine(x, y, x - horizontalSpacing, y + 80, RED);
-			//dras the left child
-			draw(currentNode->getLeft(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
-		}
-
-		//draws the right child if this nod has one
-		if (currentNode->hasRight())
-		{
-			//draws a line between this child and the curent code
-			DrawLine(x, y, x + horizontalSpacing, y + 80, RED);
-			//draws the right child
-			draw(currentNode->getright(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
-		}
-		//draws the current node
-		currentNode->draw(x, y, x(selected == currentNode));
-
-	}
-}
 
 template<typename T>
 inline BinaryTree<T>::BinaryTree()
@@ -110,4 +76,42 @@ inline void BinaryTree<T>::insert(T value)
 template<typename T>
 inline void BinaryTree<T>::remove(T value)
 {
+}
+
+template<typename T>
+inline bool BinaryTree<T>::findNode(T searchValue, TreeNode<T>*& nodeFound, TreeNode<T>*& nodeParent)
+{
+	return false;
+}
+
+template<typename T>
+inline void BinaryTree<T>::draw(TreeNode<T>* currentNode, int x, int y, int horizontalSpacing, TreeNode<T>* selected)
+{
+	//sets the needed space in between 
+	horizontalSpacing /= 2;
+
+	//check if the ucurrent node in null
+	if (currentNode)
+	{
+		//draws the left cilkd if this node has one
+		if (currentNode->hasLeft())
+		{
+			//Darws a line betwen the left child and the ucrrent node
+			DrawLine(x, y, x - horizontalSpacing, y + 80, RED);
+			//dras the left child
+			draw(currentNode->getLeft(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+
+		//draws the right child if this node has one
+		if (currentNode->hasRight())
+		{
+			//draws a line between this child and the curent code
+			DrawLine(x, y, x + horizontalSpacing, y + 80, RED);
+			//draws the right child
+			draw(currentNode->getright(), x - horizontalSpacing, y + 80, horizontalSpacing, selected);
+		}
+		//draws the current node
+		currentNode->draw(x, y, x(selected == currentNode));
+
+	}
 }
